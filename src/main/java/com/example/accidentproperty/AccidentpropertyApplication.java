515f56605a5e69110403ab8dd7c4f048b62f78bb
丹羽雄大 <yudai@niwayuuBookpuro.AirPort>
2018-11-04 19:03:02 +0900

    first commit

---
+++ src/main/java/com/example/accidentproperty/AccidentpropertyApplication.java
@@ -0,0 +1,12 @@
+package com.example.accidentproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentpropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccidentpropertyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/accidentproperty/AccidentpropertyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.accidentproperty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccidentpropertyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
