542af12e2974b29e608c111dd4d56a6229ceb8d3
丹羽雄大 <yudai@niwayuuBookpuro.AirPort>
2018-11-04 21:24:17 +0900

    リファクタリング

--- src/main/java/com/example/accidentproperty/Controller/HelloController.java
+++ src/main/java/com/example/accidentproperty/Controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.example.accidentproperty.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
