b54abf25d265ab529f5ebb8dcc36308f387ba869
丹羽雄大 <yudai@niwayuuBookpuro.AirPort>
2018-11-04 21:20:30 +0900

    htmlでhelloの表示

--- src/main/java/com/example/accidentproperty/Controller/HelloController.java
+++ src/main/java/com/example/accidentproperty/Controller/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.accidentproperty.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/top")
     public String hello() {
-        return "hello world";
+        return "top_pc";
     }
 
 }
